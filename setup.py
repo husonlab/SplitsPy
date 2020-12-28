3e95877ef577f9e90b9b82b65a4c1fd9ad143069
Daniel <daniel.huson@uni-tuebingen.de>
2020-12-28 14:21:55 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='SplitsPy',
+    version='0.1',
+    packages=['splitspy', 'splitspy.nnet', 'splitspy.graph', 'splitspy.splits', 'splitspy.outline'],
+    url='http://github.com/husonlab/SplitsPhy',
+    license='GPL',
+    author='Daniel H. Huson',
+    author_email='daniel.huson@uni-tuebingen.de',
+    description='Phylogenetic outlines'
+)
