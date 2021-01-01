ebcc8e13c2cd3be5de6d865edf5d3609464cb757
Daniel <daniel.huson@uni-tuebingen.de>
2021-01-01 09:19:21 +0100

    new version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ GPL (http://www.gnu.org/licenses/gpl.html).
 import setuptools
 
 setuptools.setup(name='SplitsPy',
-                 version='0.0.4',
+                 version='0.0.5-alpha',
                  packages=setuptools.find_packages(),
                  url='http://github.com/husonlab/SplitsPhy',
                  license='GPL',
