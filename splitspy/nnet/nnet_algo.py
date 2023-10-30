7ae7a5658fb4840961737c7d25f133a0e6846d4f
Amos Onn <amosonn@gmail.com>
2023-10-30 19:09:49 +0100

    Make info prints in neighbor_net optional

--- splitspy/nnet/nnet_algo.py
+++ splitspy/nnet/nnet_algo.py
@@ -16,20 +16,27 @@ __author__ = "David J. Bryant and Daniel H. Huson"
 from splitspy.nnet import nnet_cycle, nnet_splits, nnet_splits_lp
 
 
-def neighbor_net(labels: [str], mat: [[float]], cutoff=0.0001, mode: str = "CLS") -> Tuple[list, list]:
-    a = time.perf_counter()
-    cycle = nnet_cycle.compute(labels, mat)
-    b = time.perf_counter()
-    print(f"Computed circular order in {b-a:0.4f} seconds")
+def neighbor_net(labels: [str], mat: [[float]], cutoff=0.0001, mode: str = "CLS", verbose: bool = True) -> Tuple[list, list]:
+    if verbose:
+        a = time.perf_counter()
 
-    a = time.perf_counter()
+    cycle = nnet_cycle.compute(labels, mat)
+
+    if verbose:
+        b = time.perf_counter()
+        print(f"Computed circular order in {b-a:0.4f} seconds")
+
+    if verbose:
+        a = time.perf_counter()
+
     if mode == "LP":
         splits = nnet_splits_lp.compute(len(labels), mat, cycle, cutoff)
     else:
         constrained = (mode != "OLS")
         splits = nnet_splits.compute(len(labels), mat, cycle, cutoff, constrained)
 
-    b = time.perf_counter()
-    print(f"Computed splits in {b-a:0.4f} seconds (mode={mode})")
+    if verbose:
+        b = time.perf_counter()
+        print(f"Computed splits in {b-a:0.4f} seconds (mode={mode})")
 
     return cycle, splits
